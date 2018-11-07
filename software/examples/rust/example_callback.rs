c110c85fc47bdd1f51ffb3ae42dfa39de43aa659
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:25:51 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for heart rate events.
-    let heart_rate_receiver = hr.get_heart_rate_receiver();
+    let heart_rate_receiver = hr.get_heart_rate_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `hr` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `hr` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for heart_rate in heart_rate_receiver {
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get threshold receivers with a debounce time of 10 seconds (10000ms).
     hr.set_debounce_period(10000);
 
-    // Create receiver for heart rate reached events.
-    let heart_rate_reached_receiver = hr.get_heart_rate_reached_receiver();
+    let heart_rate_reached_receiver = hr.get_heart_rate_reached_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `hr` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `hr` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for heart_rate_reached in heart_rate_reached_receiver {
