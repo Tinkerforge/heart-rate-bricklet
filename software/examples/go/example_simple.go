21c8bc56c97738ffd69eb1824f32b5a1077466d9
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:09:26 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/heart_rate_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Heart Rate Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	hr, _ := heart_rate_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current heart rate.
+	heartRate, _ := hr.GetHeartRate()
+	fmt.Printf("Heart Rate:  bpm\n", heartRate)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_threshold.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/heart_rate_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Heart Rate Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	hr, _ := heart_rate_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get threshold receivers with a debounce time of 10 seconds (10000ms).
+	hr.SetDebouncePeriod(10000)
+
+	hr.RegisterHeartRateReachedCallback(func(heartRate uint16) {
+		fmt.Printf("Heart Rate: %d bpm\n", heartRate)
+	})
+
+	// Configure threshold for heart rate "greater than 100 bpm".
+	hr.SetHeartRateCallbackThreshold('>', 100, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
