acfe123fabd5f2e8e196cd5f553e2ddd58535cb1
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:34:02 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{heart_rate_bricklet::*, ip_connection::IpConnection};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  heart_rate_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let hr = HeartRateBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let heart_rate_receiver = hr.get_heart_rate_callback_receiver();
+     let heart_rate_receiver = hr.get_heart_rate_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `hr` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for heart_rate in heart_rate_receiver {
-            println!("Heart Rate: {} bpm", heart_rate);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `hr` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for heart_rate in heart_rate_receiver {           
+                		println!("Heart Rate: {} bpm", heart_rate);
+            }
+        });
 
-    // Set period for heart rate receiver to 1s (1000ms).
-    // Note: The heart rate callback is only called every second
-    //       if the heart rate has changed since the last call!
-    hr.set_heart_rate_callback_period(1000);
+		// Set period for heart rate receiver to 1s (1000ms).
+		// Note: The heart rate callback is only called every second
+		//       if the heart rate has changed since the last call!
+		hr.set_heart_rate_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
