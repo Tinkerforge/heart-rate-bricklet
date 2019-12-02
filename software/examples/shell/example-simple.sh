5ca8fe67f2fad5d260c1edede9218842aff8ed7c
Matthias Bolte <matthias@tinkerforge.com>
2019-12-02 18:19:59 +0100

    Remove examples

--- software/examples/shell/example-simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Connects to localhost:4223 by default, use --host and --port to change this
-
-uid=XYZ # Change XYZ to the UID of your Heart Rate Bricklet
-
-# Get current heart rate
-tinkerforge call heart-rate-bricklet $uid get-heart-rate
